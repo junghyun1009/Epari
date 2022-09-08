051fe244c6c3ccf395f90de6369976e882af8a60
junghyun1009 <junghyunk1009@gmail.com>
2022-09-08 11:14:47 +0900

    [S07P22A201-32][BE] Feat: add plant table

--- backend/django/plantbook/models.py
+++ backend/django/plantbook/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Plant(models.Model):
-    pass
+    plantId = models.AutoField(primary_key=True)
+    plantName = models.CharField(max_length=50)
+    disabledIconUrl = models.CharField(max_length=200)
+    activeIconUrl = models.CharField(max_length=200)
+    detailPictureUrl = models.CharField(max_length=500)
+    plantDescription = models.TextField()
 
 class Collect(models.Model):
     pass
