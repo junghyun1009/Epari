7ae401560fb33f4e99a4ba36fcb48ecb473aa4e6
junghyun1009 <junghyunk1009@gmail.com>
2022-09-08 10:54:55 +0900

    [S07P22A201-18][BE] Chore: start app

--- backend/django/epari_backend/settings.py
+++ backend/django/epari_backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'plantbook',
 
     'django_extensions',
     'rest_framework',
---
+++ backend/django/plantbook/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Plant(models.MOdel):
+
+class Collect(models.Model):
