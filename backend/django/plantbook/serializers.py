cd4844bef9d4f1e69ea3ed64cba7a60146751a7d
kyong0409 <kyong0409@nate.com>
2022-09-13 13:43:38 +0900

    [S07P22A201-78][BE] Feat : set plant detail serializer
    식물 상세정보 조회 serializer 설정

--- backend/django/plantbook/serializers.py
+++ backend/django/plantbook/serializers.py
@@ -6,4 +6,12 @@ class PlantListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Plant
-        fields = '__all__'
+        #fields = '__all__'
+        exclude = ('detailPictureUrl', 'plantDescription', )
+
+class PlantSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Plant
+        #fields = '__all__'
+        exclude = ('disabledIconUrl', 'activeIconUrl', )
