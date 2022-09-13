6eaf78f53427e9c0265a0417e1425d7b54a5a75e
kyong0409 <kyong0409@nate.com>
2022-09-13 14:26:15 +0900

    [S07P22A201-80][BE] Feat : modify plant & plant list  serializers
    plant & plantlist serializer 수정

--- backend/django/plantbook/serializers.py
+++ backend/django/plantbook/serializers.py
@@ -1,17 +1,27 @@
 from socket import ntohl
 from rest_framework import serializers
-from .models import Plant
+from .models import Collect, Plant
 
 class PlantListSerializer(serializers.ModelSerializer):
+    collectionCnt = serializers.IntegerField(source='collection.count', read_only=True)
 
     class Meta:
         model = Plant
-        #fields = '__all__'
-        exclude = ('detailPictureUrl', 'plantDescription', )
+        # fields = '__all__'
+        fields = ('plantId', 'plantName', 'disabledIconUrl', 'activeIconUrl', 'collectionCnt', )
+        # exclude = ('detailPictureUrl', 'plantDescription', )
 
 class PlantSerializer(serializers.ModelSerializer):
 
+    class CollectionSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = Collect
+            fields = '__all__'
+
+    collection = CollectionSerializer(many=True, read_only=True)
+
     class Meta:
         model = Plant
-        #fields = '__all__'
-        exclude = ('disabledIconUrl', 'activeIconUrl', )
+        fields = ('plantId', 'plantName', 'detailPictureUrl', 'plantDescription', 'collection')
+        # exclude = ('disabledIconUrl', 'activeIconUrl', )
