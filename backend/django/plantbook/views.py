1c43405f5db8e4b9e3d4246556be13fd93a3019f
kyong0409 <kyong0409@nate.com>
2022-09-13 13:22:37 +0900

    [S07P22A201-78][BE] Feat : set plant list views
    도감 전체 조회 함수 설정

--- backend/django/plantbook/views.py
+++ backend/django/plantbook/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Plant, Collect
+from .serializers import PlantListSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def plant_list_or_create(request):
+
+    def plant_list():
+        plants = Plant.objects.all()
+        serializer = PlantListSerializer(plants)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if request.method == 'GET':
+        return plant_list()
