73c5621140d50a1a0a6051865fb1d17ca6b9130f
kyong0409 <kyong0409@nate.com>
2022-09-21 15:19:37 +0900

    [#S07P22A201-138][BE] Feat : set titles views
    칭호 기능 구현(추후 로그인 기능 필요)
    테스트코드 작성

--- backend/django/titles/tests.py
+++ backend/django/titles/tests.py
@@ -13,26 +13,27 @@ factory = APIRequestFactory()
 class TitlesTest(APITestCase):
     def setUp(self):
         self.url = reverse('titles:titles', kwargs={ 'userId': 1 })
-        self.user = User.objects.create(username="test", password="1234")
-        self.title1 = Title.objects.create(titleId=1, titleName="칭호 없음", titleDescription="획득한 칭호 없음", titlePictureUrl="")
-        self.title2 = Title.objects.create(titleId=2, titleName="심봤다", titleDescription="당신은 산삼킬러", titlePictureUrl="")
-        self.title3 = Title.objects.create(titleId=3, titleName="도라지 마스터", titleDescription="당신은 도라지킬러", titlePictureUrl="")
-        self.obtained1 = Obtained.objects.create(titleId=1, userId=1)
-        self.obtained2 = Obtained.objects.create(titleId=2, userId=1)
+        self.title1 = Title.objects.create(titleId=1, titleName="칭호 없음", titleDescription="획득한 칭호 없음", tiltePictureUrl="")
+        self.title2 = Title.objects.create(titleId=2, titleName="심봤다", titleDescription="당신은 산삼킬러", tiltePictureUrl="")
+        self.title3 = Title.objects.create(titleId=3, titleName="도라지 마스터", titleDescription="당신은 도라지킬러", tiltePictureUrl="")
+        self.user = User.objects.create(id=1, username="test", password="1234")
+        self.obtained1 = Obtained.objects.create(titleId=self.title1, userId=self.user)
+        self.obtained2 = Obtained.objects.create(titleId=self.title2, userId=self.user)
 
     
     # 사용자 별 전체 칭호 조회 성공
     def test_get_all_titles_success(self):
         user = User.objects.get(id=1)
+        print(user)
         request = factory.get(self.url)
         force_authenticate(request, user=user)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 200)
 
     # 사용자 별 전체 칭호 조회 실패: 로그인 안됐을 때
     def test_get_all_titles_fail(self):
         request = factory.get(self.url)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 401)
 
     # 대표 칭호 등록 성공
@@ -43,7 +44,7 @@ class TitlesTest(APITestCase):
         user = User.objects.get(id=1)
         request = factory.put(self.url, data)
         force_authenticate(request, user=user)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 200)
     
     # 대표 칭호 등록 실패: 로그인 안됐을 때
@@ -52,7 +53,7 @@ class TitlesTest(APITestCase):
             'titleId': 2
         }
         request = factory.put(self.url, data)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 401)
 
     # 대표 칭호 등록 실패: 없는 칭호 요청을 보냈을 때
@@ -63,7 +64,7 @@ class TitlesTest(APITestCase):
         user = User.objects.get(id=1)
         request = factory.put(self.url, data)
         force_authenticate(request, user=user)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 404)
 
     # 대표 칭호 등록 실패: 획득하지 않은 칭호를 대표 칭호로 등록하려고 할 때
@@ -74,7 +75,7 @@ class TitlesTest(APITestCase):
         user = User.objects.get(id=1)
         request = factory.put(self.url, data)
         force_authenticate(request, user=user)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 404)
     
     # 대표 칭호 등록: 이미 대표 칭호로 등록된 칭호를 다시 등록하려고 할 때
@@ -85,9 +86,9 @@ class TitlesTest(APITestCase):
         user = User.objects.get(id=1)
         request = factory.put(self.url, data)
         force_authenticate(request, user=user)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.message, '이미 등록된 칭호입니다')        
+        self.assertEqual(response.data['message'], '이미 등록된 칭호입니다.')        
 
     # 새로운 칭호 획득 성공
     def test_obtain_title_success(self):
@@ -97,7 +98,7 @@ class TitlesTest(APITestCase):
         user = User.objects.get(id=1)
         request = factory.post(self.url, data)
         force_authenticate(request, user=user)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 201)
 
     # 새로운 칭호 획득 실패: 로그인 안됐을 때
@@ -106,7 +107,7 @@ class TitlesTest(APITestCase):
             'titleId': 3
         }
         request = factory.post(self.url, data)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 401)
 
     # 새로운 칭호 획득 실패: 없는 칭호 요청을 보냈을 때
@@ -117,5 +118,5 @@ class TitlesTest(APITestCase):
         user = User.objects.get(id=1)
         request = factory.post(self.url, data)
         force_authenticate(request, user=user)
-        response = titles(request)
+        response = titles(request, 1)
         self.assertEqual(response.status_code, 404)
--- backend/django/titles/views.py
+++ backend/django/titles/views.py
@@ -1,10 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from rest_framework.decorators import api_view
+from .serializers import ObtainedSerializer, TitleListSerializer
+from epari_backend import settings
+from .models import Title, Obtained
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @api_view(['GET', 'PUT', 'POST'])
 def titles(request, userId):
-    
+    titles = Obtained.objects.filter(userId=userId)
+    user = User.objects.get(id=userId)
+
     # 사용자 전체 칭호 확인
     def title_list():
-        pass
+        serializer = ObtainedSerializer(titles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def obtain_title():
+        titleId = request.data['titleId']
+        title = get_object_or_404(Title, titleId=titleId)
+        data = {
+            'userId': userId,
+            'titleId': titleId
+        }
+        serializer = ObtainedSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def change_title():
+        titleId = request.data['titleId']
+        title = get_object_or_404(Title, titleId=titleId)
+        if Obtained.objects.filter(userId=userId, titleId=title).exists():
+            user = User.objects.get(id=userId)
+            if user.titleId == title:
+                data = {
+                    'message': '이미 등록된 칭호입니다.'
+                }
+            else:
+                user.titleId = title
+                user.save()
+                data = {
+                    'message': '대표 칭호 등록이 완료되었습니다.'
+                }
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            data = {
+                'message': '획득하지 못한 칭호입니다.'
+            }
+            return Response(data, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        return title_list()
+    elif request.method == 'POST':
+        return obtain_title()
+    elif request.method == 'PUT':
+        return change_title()
