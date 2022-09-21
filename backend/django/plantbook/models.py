4db7b0625a6f9aebae8006a8632d69934c0761dd
kyong0409 <kyong0409@nate.com>
2022-09-21 15:25:20 +0900

    [#S07P22A201-138][BE] Fix : modify models
    일부 models column type 수정
    column 명 오타 수정

--- backend/django/plantbook/models.py
+++ backend/django/plantbook/models.py
@@ -3,17 +3,17 @@ from epari_backend import settings
 
 # Create your models here.
 class Plant(models.Model):
-    plantId = models.AutoField(primary_key=True)
+    plantId = models.CharField(max_length=10, primary_key=True)
     plantName = models.CharField(max_length=50)
-    disabledIconUrl = models.CharField(max_length=200)
-    activeIconUrl = models.CharField(max_length=200)
+    disabledIconUrl = models.CharField(max_length=500)
+    activeIconUrl = models.CharField(max_length=500)
     detailPictureUrl = models.CharField(max_length=500)
     plantDescription = models.TextField()
 
 class Collect(models.Model):
     collectId = models.AutoField(primary_key=True)
     plantId = models.ForeignKey(Plant, on_delete=models.CASCADE, related_name="collection")
-    collectPictureUrl = models.CharField(max_length=200)
+    collectPictureUrl = models.CharField(max_length=500)
     userId = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="collection")
     collectDate = models.DateField(auto_now_add=True)
     collectContent = models.TextField()
