b9a906628e725a919281ae0a6216c29df2b9b493
정유송 <yoosong0713@naver.com>
2022-09-25 13:28:53 +0900

    [##S07P22A201-15][FE] Refactor: AiCamera.tsx 분리

--- frontend/Epari/src/screens/AiCapture.tsx
+++ frontend/Epari/src/screens/AiCapture.tsx
@@ -1,64 +1,8 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
-import {launchImageLibrary} from 'react-native-image-picker';
-import {useRecoilState} from 'recoil';
-import {pictureImageUrl} from '../store/picturedImageUrl';
-import {useNavigation} from '@react-navigation/native';
+import AiCamera from '../components/AiCapture/AiCamera';
 
 const AiCapture: React.FC = ({}) => {
-  const navigation = useNavigation();
-  const [picturedImageUrl, setPicturedImageUrl] =
-    useRecoilState(pictureImageUrl);
-
-  const uploadImage = async () => {
-    const image = {
-      uri: '',
-      type: '',
-      name: '',
-    };
-    await launchImageLibrary({}, res => {
-      if (res.didCancel) {
-        console.log('User Cancelled image picker');
-      } else if (res.errorCode) {
-        console.log('ImagePicker Error', res.errorCode);
-      } else if (res.assets) {
-        console.log('ImagePicker data', res.assets);
-        image.type = res.assets[0].type;
-        image.uri = res.assets[0].uri;
-        image.name = res.assets[0].fileName;
-        setPicturedImageUrl(image.uri);
-        console.log('url', picturedImageUrl);
-      }
-    });
-  };
-
-  return (
-    <View style={styles.container}>
-      <Pressable>
-        <View>
-          <Text
-            style={styles.fontTest}
-            onPress={() => {
-              uploadImage();
-              navigation.navigate('AiResult');
-            }}>
-            CAMERA
-          </Text>
-        </View>
-      </Pressable>
-    </View>
-  );
+  return <AiCamera />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  fontTest: {
-    fontFamily: 'NeoDGM-Regular',
-  },
-});
 
 export default AiCapture;
