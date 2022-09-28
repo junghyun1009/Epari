b4448b0deb0c9822a1d705e58ba06f736098ec75
정유송 <yoosong0713@naver.com>
2022-09-28 14:32:22 +0900

    [#S07P22A201-190][FE] Feat: add carousel & navigation etc.

--- frontend/Epari/src/screens/AiSpareResult.tsx
+++ frontend/Epari/src/screens/AiSpareResult.tsx
@@ -1,32 +1,104 @@
 import React from 'react';
-import {Image, StyleSheet, View, Text, ScrollView} from 'react-native';
+import {Image, StyleSheet, View, Animated, Pressable} from 'react-native';
 import AiCamera from '../components/AiCapture/AiCamera';
-import {useRecoilValue} from 'recoil';
-import {capturedSubImage, picturedImage} from '../store/classification';
+import {useRecoilValue, useSetRecoilState} from 'recoil';
+import {
+  capturedSubImage,
+  picturedImage,
+  resultPlantName,
+} from '../store/classification';
+import AppText from '../components/AppText';
+import {useNavigation} from '@react-navigation/native';
 
 const AiSpareResult: React.FC = () => {
+  const navigation = useNavigation();
   const picturedImageState = useRecoilValue(picturedImage);
   const capturedSubImageState = useRecoilValue(capturedSubImage);
+  const setResultPlantName = useSetRecoilState(resultPlantName);
+  const plant = useRecoilValue(resultPlantName);
 
   const picturedImageUrl = picturedImageState.uri;
-  const priorSubImageUrl = capturedSubImageState[0].detailPictureUrl;
-  const secondarySubImageUrl = capturedSubImageState[1].detailPictureUrl;
-  console.log(capturedSubImageState);
+  // const priorSubImageUrl = capturedSubImageState[0].detailPictureUrl;
+  // const secondarySubImageUrl = capturedSubImageState[1].detailPictureUrl;
+  console.log('capture', capturedSubImageState);
+
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
+  const setPlantName = (item: any) => {
+    setResultPlantName(item.plantName);
+  };
 
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <View>
-          <Image source={{uri: picturedImageUrl}} style={styles.image} />
-        </View>
-        <ScrollView>
-          <Image source={{uri: priorSubImageUrl}} style={styles.image} />
+      <View style={styles.subContainer}>
+        <View style={styles.imageContainer}>
+          <View>
+            <Image source={{uri: picturedImageUrl}} style={styles.image} />
+          </View>
+          {/* <Image source={{uri: priorSubImageUrl}} style={styles.image} />
           <Text>{capturedSubImageState[0].plantName}</Text>
           <Image source={{uri: secondarySubImageUrl}} style={styles.image} />
-          <Text>{capturedSubImageState[1].plantName}</Text>
-        </ScrollView>
+          <Text>{capturedSubImageState[1].plantName}</Text> */}
+          <View style={styles.carousel}>
+            <Animated.FlatList
+              data={capturedSubImageState}
+              keyExtractor={(_, index) => index.toString()}
+              snapToInterval={190}
+              decelerationRate="fast"
+              showsVerticalScrollIndicator={false}
+              onScroll={Animated.event(
+                [{nativeEvent: {contentOffset: {y: scrollY}}}],
+                {useNativeDriver: true},
+              )}
+              renderItem={({item}) => {
+                return (
+                  <Pressable
+                    onPress={() => {
+                      setPlantName(item);
+                      navigation.navigate('AiRegister');
+                    }}>
+                    <Image
+                      source={{uri: item.detailPictureUrl}}
+                      style={styles.image}
+                    />
+                    <AppText style={styles.plantName}>
+                      {(item.plantName || '').split('_', 1)}
+                    </AppText>
+                  </Pressable>
+                );
+              }}
+            />
+            <View style={styles.pagination}>
+              {capturedSubImageState.map((_, index) => {
+                return <View key={index} style={[styles.dot]} />;
+              })}
+              <Animated.View
+                style={[
+                  styles.dotIndicator,
+                  {
+                    transform: [
+                      {
+                        translateY: Animated.divide(scrollY, 190).interpolate({
+                          inputRange: [0, 1],
+                          outputRange: [0, 12],
+                        }),
+                      },
+                    ],
+                  },
+                ]}
+              />
+            </View>
+          </View>
+        </View>
       </View>
-      <AiCamera style={styles.button} name={'다시 찍기'} />
+      <View style={styles.textContainer}>
+        <AppText>둘 중 일치하는 식물을 선택해주세요</AppText>
+      </View>
+      <View style={styles.textContainer}>
+        <AppText>일치하는 식물이 없나요?</AppText>
+        <AiCamera style={styles.button} name={'다시 찍기'} />
+      </View>
+      {/* <View style={styles.buttonContainer}></View> */}
     </View>
   );
 };
@@ -39,17 +111,62 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  subContainer: {
+    flex: 2,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
   imageContainer: {
-    // flex: 2,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'flex-end',
+  },
+  carousel: {
+    height: 190,
+    overflow: 'hidden',
   },
   image: {
     width: 150,
     height: 150,
     borderRadius: 12,
     margin: 8,
+    resizeMode: 'cover',
+  },
+  plantName: {
+    margin: 3,
+    textAlign: 'center',
+  },
+  pagination: {
+    position: 'absolute',
+    top: 75,
+    right: 12,
+  },
+  dot: {
+    width: 6,
+    height: 6,
+    borderRadius: 8,
+    backgroundColor: '#F6EDD9',
+    marginBottom: 6,
+  },
+  dotIndicator: {
+    width: 12,
+    height: 12,
+    borderRadius: 12,
+    borderWidth: 1,
+    backgroundColor: '#00845E',
+    borderColor: '#F6EDD9',
+    position: 'absolute',
+    top: -3,
+    right: -3,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
   button: {
     flexDirection: 'row',
