9650cbfe0accb3bb843f9304fe29a873791e01e4
정유송 <yoosong0713@naver.com>
2022-09-29 15:35:42 +0900

    [#S07P22A201-206][FE] Feat: add area selector

--- frontend/Epari/src/components/AiCapture/LocationSelector.tsx
+++ frontend/Epari/src/components/AiCapture/LocationSelector.tsx
@@ -1,31 +1,47 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, set, useEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
 import SelectList from 'react-native-dropdown-select-list';
+import parseErrorStack from 'react-native/Libraries/Core/Devtools/parseErrorStack';
 import AppText from '../AppText';
 
 const LocationSelector: React.FC = () => {
   const [category, setCategory] = useState(1);
   const [subCategory, setSubCategory] = useState('');
-  const categories = [
-    {
-      key: 1,
-      value: '서울시',
-    },
-    {
-      key: 2,
-      value: '어디?',
-    },
-  ];
+
+  const [categories, setCategories] = useState([]);
+
   const subCategories = {
-    1: [
-      {key: 1, value: '강남구'},
-      {key: 2, value: '강동구'},
-    ],
-    2: [
-      {key: 1, value: '아'},
-      {key: 2, value: '어'},
-    ],
+    1: [{key: 0, value: ''}],
   };
+  // const categories = new Array();
+  useEffect(() => {
+    locationList();
+  }, []);
+
+  const locationList = async () => {
+    console.log('????????');
+    await fetch('http://127.0.0.1:8000/epari/v1/locations/location')
+      .then(response => response.json())
+      .then(result => {
+        let tmp = 0;
+        const tmpArray = [];
+        result.map(item => {
+          // console.log(item);
+          if (tmp != item.areaId) {
+            // console.log('11', categories);
+            tmpArray.push({
+              key: item.areaId,
+              value: item.areaName,
+            });
+            tmp = item.areaId;
+            // console.log(tmp);
+          }
+        });
+        setCategories(tmpArray);
+      })
+      .catch(err => console.log);
+  };
+  console.log('cat', categories);
 
   return (
     <View style={styles.container}>
@@ -40,7 +56,6 @@ const LocationSelector: React.FC = () => {
           boxStyles={{
             backgroundColor: '#F6EDD9',
             // maxWidth: 150,
-
             // marginHorizontal: 10,
           }}
           dropdownStyles={{backgroundColor: '#F6EDD9'}}
@@ -79,12 +94,6 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
   },
 });
-// const locationList = async () => {
-//     await fetch('http://127.0.0.1:8000/epari/v1/locations/location')
-//       .then(response => response.json())
-//       .then(result => console.log(result));
-//   };
-//   locationList();
 
 // [{"areaName": "서울시", "locationId": 1, "sigunguName": "강남구"}, {"areaName": "서울시", "locationId": 2, "sigunguName":"관악구"}]
 
