c0b4db5ab0f48781a0067ea79315edeb2cd382d9
junghyun1009 <junghyunk1009@gmail.com>
2022-09-30 14:57:39 +0900

    [#S07P22A201-181][BE] Fix : change code for returning locationId

--- backend/django/plantbook/views.py
+++ backend/django/plantbook/views.py
@@ -25,9 +25,10 @@ def plant_list_or_create(request):
         print('image', userImageUrl)
 
         # areaId랑 sigunguId 받아서 locationId로 변환
-        areaId = request.data.areaId
-        sigunguId = request.data.sigunguId
-        locationId = Location.objects.filter(areaId=areaId, sigunguId=sigunguId)
+        areaId = request.data["areaId"]
+        sigunguId = request.data["sigunguId"]
+        location = Location.objects.get(areaId=areaId, sigunguId=sigunguId)
+        locationId = location.locationId
         request.data.__setitem__('locationId', locationId)
 
         def create_plant_image(userImageUrl):
