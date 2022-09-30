63ba62d371652ff0f080fafbc5c35a9b177f7453
junghyun1009 <junghyunk1009@gmail.com>
2022-09-30 14:32:09 +0900

    [#S07P22A201-181][FE] Feat : add TitleList in screen

---
+++ frontend/Epari/src/screens/TitleList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import TitleListStack from '../navigation/TitleListStack';
+
+const TitleList: React.FC = () => {
+  return <TitleListStack />;
+};
+
+export default TitleList;
