86130d98574a4b7c986ca2f66a4d501a734cc0e7
최진우 <freiya1002@naver.com>
2022-10-04 07:48:22 +0900

    [#S07P22A201-198][FE] Feat : HerbBook 코드 정리
    * MainPage 디자인 수정
    * AuthStack (로그인 토큰 보유 여부 체크 필요)
    * LoginPage 디자인 수정
    * HerbBook API 코드 작성, 연결 확인 필요

---
+++ frontend/Epari/src/components/Home/BottomButtons.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+
+type BottomProps = {
+  navigation: any;
+};
+
+const Bottom: React.FC<BottomProps> = ({navigation}) => {
+  async function movePageAI() {
+    navigation.navigate('AiCapture');
+  }
+  async function movePageBook() {
+    navigation.navigate('HerbBook');
+  }
+  return (
+    <View style={styles.bottomBox}>
+      <TouchableOpacity onPress={() => movePageAI()}>
+        <View style={styles.leftButton}>
+          <Text style={styles.font_3}>A I</Text>
+          <Text style={styles.font_2}>이 꽃이 무슨꽃인가?</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => movePageBook()}>
+        <View style={styles.rightButton}>
+          <Text style={styles.font_3}>Book</Text>
+          <Text style={styles.font_2}>내가 수집한 꽃 보여줘!</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+let ScreenWidth = Dimensions.get('window').width;
+let ScreenHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  font_1: {
+    fontFamily: 'NeoDGM-Regular',
+    color: '#FFFFFF',
+  },
+  font_2: {
+    fontFamily: 'NeoDGM-Regular',
+    color: '#FFFFFF',
+    fontSize: 15,
+    textShadowColor: 'rgb(244, 182, 255)',
+    textShadowRadius: 2,
+    textShadowOffset: {
+      width: 1.8,
+      height: 1.8,
+    },
+  },
+  font_3: {
+    fontFamily: 'NeoDGM-Regular',
+    color: '#FFFFFF',
+    fontSize: 75,
+    textShadowColor: 'rgb(244, 182, 255)',
+    textShadowRadius: 2,
+    textShadowOffset: {
+      width: 1.8,
+      height: 1.8,
+    },
+  },
+  bottomBox: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    marginTop: ScreenHeight * 0.1,
+    marginBottom: -50,
+  },
+  leftButton: {
+    width: ScreenWidth * 0.42,
+    height: ScreenWidth * 0.38,
+    // backgroundColor: 'rgb(152, 180, 234)',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 11,
+    borderWidth: 7,
+    borderColor: 'rgb(151, 141, 255)',
+    left: -ScreenWidth * 0.09,
+  },
+  rightButton: {
+    width: ScreenWidth * 0.42,
+    height: ScreenWidth * 0.38,
+    // backgroundColor: 'rgb(152, 180, 234)',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 11,
+    borderWidth: 7,
+    borderColor: 'rgb(151, 141, 255)',
+    right: -ScreenWidth * 0.09,
+  },
+});
+export default Bottom;
