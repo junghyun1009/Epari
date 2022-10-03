14c89eae1b1615a96d4ba3cb5122d05ce08f8810
junghyun1009 <junghyunk1009@gmail.com>
2022-10-03 15:51:03 +0900

    [#S07P22A201-90][FE] Style : TotalTitle.tsx

--- frontend/Epari/src/components/Title/Lists/TotalTitle.tsx
+++ frontend/Epari/src/components/Title/Lists/TotalTitle.tsx
@@ -1,8 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, Image, Dimensions} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  Dimensions,
+  Pressable,
+} from 'react-native';
 import {AppStackParamList} from '../../../types';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
-import TitleItem from './TitleItem';
+import AppText from '../../AppText';
 
 export type TotalListScreenProps = NativeStackScreenProps<
   AppStackParamList,
@@ -35,13 +42,30 @@ const TotalTitle: React.FC<TotalListScreenProps> = () => {
     <View>
       <View style={styles.container}>
         {titles.map(title => (
-          <View key={title.titleId.titleId}>
+          <View key={title.titleId.titleId} style={styles.Item}>
             <Image
               source={{uri: title.titleId.titlePictureUrl}}
               style={styles.ImageItem}
             />
-            <Text>{title.titleId.titleName}</Text>
-            <Text>{title.titleId.titleDescription}</Text>
+            <View style={styles.Text}>
+              <AppText style={styles.TitleItem}>
+                {title.titleId.titleName}
+              </AppText>
+              <AppText style={styles.TextItem}>
+                {title.titleId.titleDescription}
+              </AppText>
+            </View>
+            <Pressable>
+              <View>
+                <AppText
+                  style={styles.button}
+                  onPress={() => {
+                    saveImage();
+                  }}>
+                  등록하기
+                </AppText>
+              </View>
+            </Pressable>
           </View>
         ))}
       </View>
@@ -50,19 +74,57 @@ const TotalTitle: React.FC<TotalListScreenProps> = () => {
 };
 
 let ScreenWidth = Dimensions.get('window').width;
+let ScreenHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexWrap: 'wrap',
-    flexDirection: 'column',
     overflow: 'scroll',
     justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#FFF7F2',
   },
+  Item: {
+    height: ScreenWidth * 0.23,
+    width: ScreenWidth * 0.95,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: ScreenHeight * 0.015,
+    marginHorizontal: ScreenWidth * 0.025,
+    borderRadius: 12,
+    borderWidth: 2.4,
+    backgroundColor: '#CFDE8B',
+  },
   ImageItem: {
-    width: ScreenWidth * 0.13,
-    height: ScreenWidth * 0.13,
+    width: ScreenWidth * 0.16,
+    height: ScreenWidth * 0.16,
+    borderRadius: 12,
+    borderWidth: 2.4,
+    borderColor: 'black',
+    backgroundColor: '#FFFFFF',
+    marginLeft: ScreenWidth * 0.025,
+  },
+  Text: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: ScreenWidth * 0.025,
+    justifyContent: 'center',
+  },
+  TitleItem: {
+    fontSize: 18,
+    marginBottom: ScreenHeight * 0.005,
+  },
+  TextItem: {
+    width: ScreenWidth * 0.5,
+  },
+  button: {
+    padding: ScreenWidth * 0.02,
+    backgroundColor: '#00845E',
+    borderRadius: 10,
+    borderWidth: 2,
+    textAlign: 'center',
   },
 });
 
