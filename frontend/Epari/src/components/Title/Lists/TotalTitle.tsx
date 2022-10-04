3fbc7de4b94a6987dcdbc6b5d1fae800a79785b9
junghyun1009 <junghyunk1009@gmail.com>
2022-10-04 13:38:08 +0900

    [#S07P22A201-181] Feat : add conditions for titles

--- frontend/Epari/src/components/Title/Lists/TotalTitle.tsx
+++ frontend/Epari/src/components/Title/Lists/TotalTitle.tsx
@@ -10,6 +10,7 @@ import {
 import {AppStackParamList} from '../../../types';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import AppText from '../../AppText';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export type TotalListScreenProps = NativeStackScreenProps<
   AppStackParamList,
@@ -18,56 +19,146 @@ export type TotalListScreenProps = NativeStackScreenProps<
 
 const TotalTitle: React.FC<TotalListScreenProps> = () => {
   const [titles, setTitles] = useState([]);
+  const [token, setToken] = useState('');
+  const [collected, setCollected] = useState(0);
 
   useEffect(() => {
-    titleList();
+    // titleList();
+    getData();
+    // getCollection();
   }, []);
+  useEffect(() => {
+    getCollection();
+  }, [token]);
 
-  const titleList = () => {
-    fetch('http://127.0.0.1:8000/epari/v1/titles')
+  const getData = async () => {
+    try {
+      const storedToken = await AsyncStorage.getItem('GoogleAccessToken');
+      if (storedToken !== null) {
+        console.log('storedToken : ', storedToken);
+        setToken(storedToken);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  console.log('토큰', token);
+
+  const getCollection = () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    };
+    fetch('http://j7a201.p.ssafy.io/epari/v1/collection/', requestOptions)
       .then(response => response.json())
       .then(result => {
-        console.log(0, result);
-        // let tmpTitleArray = [];
-        // result.map(item => {
-        //   tmpTitleArray.push(item);
-        // });
-        // console.log(1, tmpTitleArray);
-        setTitles(result);
-        console.log(2, titles);
+        console.log('내꺼', result);
+        const collected = 0;
+        result.map(item => {
+          if (item.isCollected === true) {
+            console.log('모은 거', item);
+            collected += 1;
+
+            if (collected === 1) {
+              const obtained = new FormData();
+              obtained.append('titleId', 1);
+
+              const requestPost = {
+                method: 'POST',
+                body: obtained,
+                headers: {
+                  'Content-Type': 'multipart/form-data',
+                  Authorization: token,
+                },
+              };
+              fetch('http://j7a201.p.ssafy.io/epari/v1/titles/', requestPost)
+                .then(response => {
+                  response.text();
+                })
+                .then(result => {
+                  console.log('1111111111', result);
+                  console.log(obtained);
+                });
+            }
+
+            return;
+          }
+        });
+
+        console.log('토큰zzzzz', token);
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+          },
+        };
+        fetch('http://j7a201.p.ssafy.io/epari/v1/titles', requestOptions)
+          .then(response => response.json())
+          .then(result => {
+            console.log(0, result);
+            // let tmpTitleArray = [];
+            // result.map(item => {
+            //   tmpTitleArray.push(item);
+            // });
+            // console.log(1, tmpTitleArray);
+            const resultSet = new Set(result);
+            const newResult = [...resultSet];
+            console.log('newResult', newResult);
+            setTitles(newResult);
+            console.log(2, titles);
+          });
       });
   };
 
   return (
     <View>
       <View style={styles.container}>
-        {titles.map(title => (
-          <View key={title.titleId.titleId} style={styles.Item}>
-            <Image
-              source={{uri: title.titleId.titlePictureUrl}}
-              style={styles.ImageItem}
-            />
-            <View style={styles.Text}>
-              <AppText style={styles.TitleItem}>
-                {title.titleId.titleName}
-              </AppText>
-              <AppText style={styles.TextItem}>
-                {title.titleId.titleDescription}
-              </AppText>
-            </View>
-            <Pressable>
-              <View>
-                <AppText
-                  style={styles.button}
-                  onPress={() => {
-                    saveImage();
-                  }}>
-                  등록하기
+        <Pressable>
+          <View>
+            <AppText
+              style={styles.button}
+              onPress={() => {
+                titleList();
+              }}>
+              칭호 불러오기
+            </AppText>
+          </View>
+        </Pressable>
+        {titles.length ? (
+          titles.map(title => (
+            <View key={title.titleId.titleId} style={styles.Item}>
+              <Image
+                source={{uri: title.titleId.titlePictureUrl}}
+                style={styles.ImageItem}
+              />
+              <View style={styles.Text}>
+                <AppText style={styles.TitleItem}>
+                  {title.titleId.titleName}
+                </AppText>
+                <AppText style={styles.TextItem}>
+                  {title.titleId.titleDescription}
                 </AppText>
               </View>
-            </Pressable>
-          </View>
-        ))}
+              <Pressable>
+                <View>
+                  <AppText
+                    style={styles.button}
+                    onPress={() => {
+                      // saveImage();
+                    }}>
+                    등록하기
+                  </AppText>
+                </View>
+              </Pressable>
+            </View>
+          ))
+        ) : (
+          <AppText>아직 획득한 칭호가 없습니다.</AppText>
+        )}
       </View>
     </View>
   );
